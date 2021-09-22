496f1390fe8736b2ffb689419aba590f18f483cd
Zeb <zebgosik@yahoo.com>
2021-09-21 22:27:58 -0400

    Ch 6 Unit Testing Exercises.

--- src/org/launchcode/java/demos/lsn5unittesting/main/Main.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Main.java
@@ -1,9 +1,14 @@
 package org.launchcode.java.demos.lsn5unittesting.main;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         Car car = new Car("Toyota", "Prius", 10, 50);
         System.out.println(car.getMake() + " - " + car.getModel());
     }
+
+
+
 }
