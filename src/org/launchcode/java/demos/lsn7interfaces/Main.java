aa4a8edbbf3444b871e29ddc63fe4c6c93949853
Zeb <zebgosik@yahoo.com>
2021-09-28 22:32:41 -0400

    Ch 8 Interfaces -Exercises.

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,18 +1,27 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+        Comparator coneComparator = new ConeComparator();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
-
+        System.out.println(flavors);
+        flavors.sort(comparator);
+        System.out.println(flavors);
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        System.out.println(cones);
+        cones.sort(coneComparator);
+        System.out.println(cones);
+
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
